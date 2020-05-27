f75a52185b36c86508fa633aa1072425b64851f9
FerideM <56362389+FerideM@users.noreply.github.com>
2020-05-28 01:33:39 +0300

    Add files via upload

---
+++ Language/exampleDescStat.lisp
@@ -0,0 +1,20 @@
+﻿(require "./languageDescStat.lisp")
+
+(! 5 7 3 6 32
+= mean)
+
+(! 3 4 5
+= mean 
+= median 
+= sd 
+= se 
+= var)
+
+(! 23 3 5 6 
+= mean
+= median 
+= disp)
+
+(! 5 3 7 4.86 8 10 4 9 13 0 4 0 5 12 5 5 2.35
+= disp
+= range)
